ffe57b4a02aeb13be65e03d1e62637d84f81c5e2
Venatum <vincentlequec@gmail.com>
2023-05-06 10:55:03 +0200

    chore(deps): update bullmq to v3.12.1

--- release.config.js
+++ release.config.js
@@ -56,7 +56,7 @@ module.exports = {
 			]
 		}],
 		['@semantic-release/release-notes-generator', {preset: 'conventionalcommits'}],
-		'@semantic-release/github',
 		'@semantic-release/git',
+		'@semantic-release/github',
 	],
 };
